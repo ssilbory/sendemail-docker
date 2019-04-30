97bb94bc25b65162c2a67c27ffa4d3296f8010b0
Samuel Flory Silbory <sam@silbory.com>
2019-04-30 13:03:42 -0700

    Update help

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
-#!/bin/sh -x
+#!/bin/sh 
+help(){
+    echo 'The script requires the following arguments To From Subject "Body" Server.'
+    echo "Optionally you can supply a user, password and additional arguments to sendEmail."
+    echo
+    echo 'Examples:'
+    echo 'docker run ssilbory/sendemail junkmail@silbory junkmail@silbory.com "Hey" "Important infomation!" smtp.silbory.com'
+    echo 'docker run ssilbory/sendemail junkmail@silbory junkmail@silbory.com "Hey" "Important infomation!" smtp.siblory sam' "'\$vaqqpgq!znnnn'"
+    echo 'docker run ssilbory/sendemail junkmail@silbory junkmail@silbory.com "Hey" "Important infomation!" smtp.gmail.com:587 sam.silbory' "'\$vaqmqpgq!znnnn' '-o tls=yes'"
+    echo "Note that to use gmail in the above example you need to create an app password for gmail, and force tls."
+    exit 1
+}
+
+[ "$1" = "-h" ] && help
+
 TO=$1
 FROM=$2
 SUBJECT=$3
@@ -11,4 +25,3 @@ if [ -n "$7" ];then
 else
   sendEmail -t "$TO" -f "$FROM" -u \""$SUBJECT"\" -m \""${BODY}"\" -s "$SERVER" $6
 fi
-     
